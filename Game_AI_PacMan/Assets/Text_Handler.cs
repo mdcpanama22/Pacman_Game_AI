42a425ee2c016a96814e3c32c0ebd021bd78f3be
mdcpanama22 <mdcpanama22@gmail.com>
2018-09-11 19:07:23 -0400

    initial commit. includes text reading, so when you run the AI_Maze_testing scene, you will see the output of the text file in the console

---
+++ Game_AI_PacMan/Assets/Text_Handler.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text.RegularExpressions;
+
+public class Text_Handler : MonoBehaviour {
+    public TextAsset MAZE;
+    // Use this for initialization
+    void Start() {
+        ReadString();
+    } 
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    static void WriteString()
+    {
+
+    }
+
+    public void ReadString()
+    {
+        string maze = MAZE.text;
+        string[] mLines = Regex.Split(maze, "\n");
+
+        List<string[]> Maze = new List<string[]>();
+
+        for(int i = 0; i < mLines.Length; i++)
+        {
+            string line = mLines[i];
+            string[] character = Regex.Split(line, " ");
+
+            Maze.Add(character);
+        }
+
+        string LINE = "";
+        for(int i = 0; i < Maze.Count; i++)
+        {
+            foreach(string x in Maze[i])
+            {
+                LINE += x;
+            }
+            Debug.Log(LINE + "\n");
+            LINE = "";
+
+        }
+    }
+}
